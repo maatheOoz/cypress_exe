9042174228923dd036cd0ea755edc2d16588d2e4
maatheOoz <matheussantosbmth@gmail.com>
2024-10-31 23:34:01 -0300

    commitando

---
+++ cypress/e2e/ebac-jobs/contatos.cy.js
@@ -0,0 +1,28 @@
+/// <reference types='cypress' />
+
+describe("Teste para a home", () => {
+  beforeEach(() => {
+    cy.visit("https://agenda-contatos-react.vercel.app/");
+  });
+  it("Deve incluir informações no formulário", () => {
+    cy.get('input[type="text"]').type("Matheus Vieira"); 
+    cy.get('input[type="email"]').type("matheusv@gmail.com"); 
+    cy.get('input[type="tel"]').type("11012345678"); 
+    cy.contains("Adicionar").click(); 
+    cy.screenshot("teste-incluir"); 
+  });
+
+  it("Deve alterar a informação da lista", () => {
+    cy.get(":nth-child(2) > .sc-gueYoa > .edit").click(); 
+    cy.get('input[type="text"]').clear().type("Daniel Rodrigues"); 
+    cy.get('input[type="email"]').clear().type("danielr@hotmail.com"); 
+    cy.get('input[type="tel"]').clear().type("11987654321"); 
+    cy.get(".alterar").click(); 
+    cy.screenshot("teste-editar"); 
+  });
+
+  it("Deve remover um contato da lista", () => {
+    cy.get(":nth-child(3) > .sc-gueYoa > .delete").click(); 
+    cy.screenshot("teste-remover"); 
+  });
+});
